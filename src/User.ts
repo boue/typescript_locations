97b4aef0d519daf7955e9b875194d987cce44ab1
boue <boue.germain@gmail.com>
2019-09-08 10:50:46 -0700

    Complete initial implementation of User using faker to generate fake data

---
+++ src/User.ts
@@ -0,0 +1,19 @@
+import faker from "faker";
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    //responsible for initialisation of obj location
+    this.location = {
+      //faker messed up and have lat and lon as strings
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+}
